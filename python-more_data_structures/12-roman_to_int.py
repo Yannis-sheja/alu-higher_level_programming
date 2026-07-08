959b78802b454278bfbae65297f90aae5e729fab
yann <y.sheja@alustudent.com>
2026-07-08 14:08:32 -0700

    Revise

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,9 @@ def roman_to_int(roman_string):
     total = 0
     for i in range(len(roman_string)):
         current = values.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and current < values.get(roman_string[i + 1], 0):
+        next_val = values.get(roman_string[i + 1], 0) \
+            if i + 1 < len(roman_string) else 0
+        if current < next_val:
             total -= current
         else:
             total += current
