6a4d2ab0ef00a6ecc3bc9a1118b609396a25b702
yann <y.sheja@alustudent.com>
2026-07-28 09:02:18 -0700

    qtn 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module helps to access and update private attribute"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+
+        self.__size = size
+
+    @property
+    def size(self):
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+            self.__size = value
+
+    def area(self):
+
+        return self.__size * self.__size
