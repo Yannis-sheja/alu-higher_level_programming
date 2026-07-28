4418993cff70debe46d2a0c72ea9184e56d9d0fa
yann <y.sheja@alustudent.com>
2026-07-28 06:18:40 -0700

    Qtn 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a square with size as a private instance atrribute"""
+
+class Square: 
+    """Represents a square"""
+
+    def __init__(self, size):
+        self.size = size 
