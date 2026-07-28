ce88c298173fccebafce868e1125e2ab390d9a09
yann <y.sheja@alustudent.com>
2026-07-28 06:36:01 -0700

    Solved 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a square with size as a private instance atrribute"""
 
-class Square: 
+
+class Square:
     """Initialize a square"""
 
     def __init__(self, size):
-
-        self.__size = size 
+        self.__size = size
