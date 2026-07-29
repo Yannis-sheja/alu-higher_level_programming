767f3d47d3eb95f0e37928e713d313b5ed9f23fa
yann <y.sheja@alustudent.com>
2026-07-28 21:38:38 -0700

    qtn1 solved

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,34 +11,34 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """Retrieve width"""
+    @property
+    def width(self):
+        """Retrieve width"""
 
-            return self.__width
+        return self.__width
 
-        @width.setter
-        def width(self, value):
+    @width.setter
+    def width(self, value):
 
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
 
-            self.__width = width
+        self.__width = width
 
-        @property
-        def height(self):
-            """Retrieve width"""
+    @property
+    def height(self):
+        """Retrieve width"""
 
-            return self.__height
+        return self.__height
 
-        @height.setter
-        def height(self, value):
+    @height.setter
+    def height(self, value):
 
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
 
-            self.__height = height
+        self.__height = height
