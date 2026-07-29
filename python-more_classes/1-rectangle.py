bfa928dc1adb23161641750a4798ccd66a69cf74
yann <y.sheja@alustudent.com>
2026-07-28 21:44:01 -0700

    qtn1 solved

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
 
-        self.__width = width
+        self.__value = value
 
     @property
     def height(self):
@@ -41,4 +41,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
 
-        self.__height = height
+        self.__value = value
