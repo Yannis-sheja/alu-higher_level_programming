98c699a3b48d19ebd95626a7318eb72cd1994532
yann <y.sheja@alustudent.com>
2026-07-28 22:06:57 -0700

    qtn 3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""This module defines a rectangle with its width and height and
+helps to calculate its area and perimeter"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize the new rectangle"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve width"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __init__(self):
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rows = ["#" * self.__width for _ in range(self.__height)]
+        return "\n".join(rows)
