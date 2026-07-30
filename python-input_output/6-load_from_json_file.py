ea9ab8454b0b7d39d531e59e33b99eaacf1181a1
yann <y.sheja@alustudent.com>
2026-07-30 01:34:51 -0700

    Not loads

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ def load_from_json_file(filename):
     """Create an object from a JSON file"""
 
     with open(filename, encoding="utf-8") as f:
-        return json.loads(f)
+        return json.load(f)
