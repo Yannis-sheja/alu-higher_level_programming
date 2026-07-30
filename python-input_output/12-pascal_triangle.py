2cb86653260d39e9ea3102fea8d552d60285da26
yann <y.sheja@alustudent.com>
2026-07-30 02:17:33 -0700

    qtn12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a function that generates a pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """Returns a list of integer representing a pascal's triangle"""
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
