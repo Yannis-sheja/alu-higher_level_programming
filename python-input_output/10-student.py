da875a5a93cbba5d6e9deab1e295e4908f0b7534
yann <y.sheja@alustudent.com>
2026-07-30 02:04:49 -0700

    qtn 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a student class"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialise a new student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation"""
+
+        if type(attrs) is list and all(type(a) is str for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+        return self.__dict__
