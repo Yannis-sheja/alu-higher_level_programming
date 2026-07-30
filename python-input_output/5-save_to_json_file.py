474dbf450127b4447f5baa49d846350769175753
yann <y.sheja@alustudent.com>
2026-07-30 01:25:39 -0700

    Qtn 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a function that saves an object
+to a JSON file"""
+import json
+
+
+def save_to_json_file(my_obj, fielname):
+    """Writes an Object to a text file, using JSON
+    representation"""
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dumps(my_obj, f)
