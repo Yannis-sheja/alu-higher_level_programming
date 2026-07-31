595347855c020c7dd5301689b86b46d10879158c
Yannis <y.sheja@alustudent.com>
2026-07-31 19:18:29 +0000

    Fixed

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status and prints the body details."""
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
+
 if __name__ == "__main__":
-	with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-	print("Body response:")
-   	print("\t- type: {}".format(type(body)))
-    	print("\t- content: {}".format(body))
-    	print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
