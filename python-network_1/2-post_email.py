ebf40467ecdd524a211474e10faf11c153eaf045
Yannis <y.sheja@alustudent.com>
2026-07-31 18:57:16 +0000

    Permissions

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Sends a POST request with an email parameter and prints the response body."""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read()
+    print(body.decode("utf-8"))
