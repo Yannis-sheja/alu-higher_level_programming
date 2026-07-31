fb92e8dab6e7387165e20134d997f01a9334a4b1
Yannis <y.sheja@alustudent.com>
2026-07-31 19:16:28 +0000

    Qtn 0 solved

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,10 +3,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+	with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+	print("Body response:")
+   	print("\t- type: {}".format(type(body)))
+    	print("\t- content: {}".format(body))
+    	print("\t- utf8 content: {}".format(body.decode("utf-8")))
