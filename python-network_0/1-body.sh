21f291a97f21172af1bd841ae5b3fa3b7905251a
Yannis <y.sheja@alustudent.com>
2026-07-31 08:10:26 +0000

    Sends GET request

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response
-# only if the status code is 200
-response=$(curl -s -w "\n%{http_code}" "$1")
-status_code=$(echo "$response" | tail -n1)
-body=$(echo "$response" | sed '$d')
-
-if [ "$status_code" -eq 200 ]; then
-    echo -n "$body"
-fi
+# Sends a GET request to a URL and displays the body only if the status code is 200
+curl -s -o /tmp/1-body_output -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/1-body_output
